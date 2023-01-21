d1af5fb818ffa1cc839e143aa566061290210665
Muhammed Zafar <98420006+MZaFaRM@users.noreply.github.com>
2023-01-21 22:56:50 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+var music = document.getElementById("music"); 
+
 document
   .querySelector(".container")
   .addEventListener("mouseenter", function () {
     document.querySelector("body").classList.add("bg_image");
+    music.play()
   });
 
 document
   .querySelector(".container")
   .addEventListener("mouseleave", function () {
     document.querySelector("body").classList.remove("bg_image");
+    music.pause()
   });
